5b59a721cffcf2f6606cbe3e778770bf4373b011
yoganandj <yoganandjg003@gmail.com>
2025-07-17 13:13:22 +0530

    updated deployment logic groovy

--- jenkins-pipeline-project/pipelines/deployment_logic.groovy
+++ jenkins-pipeline-project/pipelines/deployment_logic.groovy
@@ -1,27 +1,36 @@
-getCurrent(){
+def getCurrent() {
     return [
-        'envName': "${AWS_ACCOUNT}",
-        'envAwsCredentials': "${AWS_CREDENTIALS_IN_JENKINS}"
+        'envName': "${env.AWS_ACCOUNT}",
+        'envAwsCredentials': "${env.AWS_CREDENTIALS_IN_JENKINS}"
     ]
 }
 
 def execute_deploy(List ordered_stacks) {
-    stage("Execution Action"){
+    stage("Execution Action") {
         script {
-            sh "echo \"STACK: ${STACK}, INCLUDE_PREVIOUS_STACK: ${INCLUDE_PREVIOUS_STACK}, ENVIRONMENT: ${ENVIRONMENT}, AWS_REGION: ${AWS_REGION}, AWS_PROFILE: ${AWS_PROFILE}, AWS_ACCOUNT: ${AWS_ACCOUNT}, GIT_COMMIT: ${GIT_COMMIT}, GIT_BRANCH: ${GIT_BRANCH}, GIT_URL: ${GIT_URL}\""
-            sh "echo \"Ordered Status: ${ordered_stacks}\""
-            stack = ordered_stacks
+            echo """Stack Details:
+                STACK: ${env.STACK}
+                INCLUDE_PREVIOUS_STACK: ${env.INCLUDE_PREVIOUS_STACK}
+                ENVIRONMENT: ${env.ENVIRONMENT}
+                AWS_REGION: ${env.AWS_REGION}
+                AWS_PROFILE: ${env.AWS_PROFILE}
+                AWS_ACCOUNT: ${env.AWS_ACCOUNT}
+                GIT_COMMIT: ${env.GIT_COMMIT}
+                GIT_BRANCH: ${env.GIT_BRANCH}
+                GIT_URL: ${env.GIT_URL}
+            """
+            echo "Ordered Stacks: ${ordered_stacks}"
         }
 
-        if(ACTION == "DEPLOY") {
-            stacks.each { stack -> 
-                if(fileExists("src/{stack}/jenkins.groovy")) {
+        if (params.ACTION == "DEPLOY") {
+            ordered_stacks.each { stack -> 
+                if (fileExists("src/${stack}/jenkins.groovy")) {
                     def jenkinsScript = load "src/${stack}/jenkins.groovy"
                     jenkinsScript.checkUnitTests(getCurrent())
                 }
 
                 // terraform
-                if(fileExists("src/${stack}/terraform/jenkins.groovy")) {
+                if (fileExists("src/${stack}/terraform/jenkins.groovy")) {
                     def jenkinsScript = load "src/${stack}/terraform/jenkins.groovy"
                     jenkinsScript.checkAndDeploy(getCurrent())
                 } else {
@@ -29,7 +38,7 @@ def execute_deploy(List ordered_stacks) {
                 }
 
                 // resources initialization
-                if(fileExists("src/{stack}/jenkins.groovy")) {
+                if (fileExists("src/${stack}/jenkins.groovy")) {
                     def jenkinsScript = load "src/${stack}/jenkins.groovy"
                     jenkinsScript.initResources(getCurrent())
                 } else {
@@ -38,11 +47,10 @@ def execute_deploy(List ordered_stacks) {
             }
         } else {
             stage("Execution Action - No Deploy") {
-                script {
-                    sh "echo \"No deploy action taken for stack: ${stack}\""
-                }
+                echo "No deploy action taken for stack: ${ordered_stacks}"
             }
         }
     }
 }
 
+return this
